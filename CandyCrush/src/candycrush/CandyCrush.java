1b43d54d902397027345133c3fabf1c2aaa4c0c2
mohsen0321 <mohsenkhaled000@gmail.com>
2023-05-04 21:50:55 +0200

    mainwindow

--- CandyCrush/src/candycrush/CandyCrush.java
+++ CandyCrush/src/candycrush/CandyCrush.java
@@ -1,4 +1,8 @@
-package candy.crash;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package candycrush;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -10,27 +14,27 @@ import javafx.stage.Stage;
 
 /**
  *
- * @author DELL
+ * @author Desktop
  */
-public class CandyCrash extends Application {
-
+public class CandyCrush extends Application {
+    
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
-
+            
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("Mohsen");
             }
         });
-
+        
         StackPane root = new StackPane();
         root.getChildren().add(btn);
-
+        
         Scene scene = new Scene(root, 300, 250);
-
+        
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -41,9 +45,6 @@ public class CandyCrash extends Application {
      */
     public static void main(String[] args) {
         launch(args);
-        System.out.println("Omar Taha");
-        System.out.println("Omar Attia");
-        System.out.println("Kareem");
     }
-
+    
 }
