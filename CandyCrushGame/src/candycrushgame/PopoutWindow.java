305f0b6a3baf049cdfa077f4c5f6516b45654e5a
karimmohamed596 <karimmohamedali596@gmail.com>
2023-05-06 22:58:20 +0200

    modified popoutwindow

--- CandyCrushGame/src/candycrushgame/PopoutWindow.java
+++ CandyCrushGame/src/candycrushgame/PopoutWindow.java
@@ -5,22 +5,28 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class PopoutWindow extends Stage {
     public PopoutWindow(){
-          // Create the layout for the popout window
-        VBox layout = new VBox();
+         // Create the layout for the popout window
+
+        HBox layout = new HBox();
+        Button newgamebtn = new Button("New Game");
         Button closeButton = new Button("Close");
         layout.getChildren().add(closeButton);
+        layout.getChildren().add(newgamebtn);
+        
+
 
         // Create the scene for the popout window
         Scene scene = new Scene(layout, 300, 200);
 
         // Set the scene for the popout window
         this.setScene(scene);
-        this.setTitle("Popout Window");
+        this.setTitle("Candy Crush");
     }
     
 }
