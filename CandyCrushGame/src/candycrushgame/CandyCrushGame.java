7cbbc91d48cb6eb4a58d5695058deb4e546967a3
omar taha <106601504+omartaha77@users.noreply.github.com>
2023-05-13 21:19:00 +0300

    Sounds Effect

--- CandyCrushGame/src/candycrushgame/CandyCrushGame.java
+++ CandyCrushGame/src/candycrushgame/CandyCrushGame.java
@@ -1,26 +1,46 @@
 package candycrushgame;
 
 import javafx.application.Application;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 public class CandyCrushGame extends Application {
 
     @Override
+ 
     public void start(Stage primaryStage) {
-
+        // Path
+        String projectDir = System.getProperty("user.dir");
+        
+        // sound of main window 
+        String soundFile = "file:///C:/Users/omar7/Downloads/Candy.wav";
+        Media media = new Media( soundFile);
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+        // sound of game
+        String soundFile1 = "file:///C:/Users/omar7/Downloads/game.wav";
+        Media media1 = new Media(soundFile1);
+        MediaPlayer mediaPlayer1 = new MediaPlayer(media1);
+        
+        
         // create an instance of the MainWindow class  
         MainWindow mainWindow = new MainWindow();
         // create an instance of the LoadingImages class
         LoadingImages LoadingImages = new LoadingImages();
         // create an instance of the popoutWindow class
-        PopoutWindow popoutWindow = new PopoutWindow();
-        popoutWindow.show();             // Show the popout window
+//        PopoutWindow popoutWindow = new PopoutWindow();
+//        popoutWindow.show();             // Show the popout window
         
         // create a Scene object with the MainWindow as the root
+
         
         primaryStage.setScene(mainWindow.getScene1());
         mainWindow.btnnewgame().setOnAction(e->{
-        primaryStage.setScene(LoadingImages.getScene2());});
+        primaryStage.setScene(LoadingImages.getScene2());
+        mediaPlayer.stop();
+        mediaPlayer1.play();});
+        
         primaryStage.setTitle("Candy Crush");
         primaryStage.setResizable(false);
         primaryStage.show();
